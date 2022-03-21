73a11d0de4167504fec9d778b62151549d542987
Parth Chaturvedi <parthcv0101@gmail.com>
2022-03-21 12:01:54 -0700

    Add home page

---
+++ app/src/main/java/com/bcit/project_genuity/UserActivity.java
@@ -0,0 +1,14 @@
+package com.bcit.project_genuity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
