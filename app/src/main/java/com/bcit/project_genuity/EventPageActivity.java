15296a25bc3d717ba2e60b20742fda6798d2b3c6
Verridium <77175850+Verridium@users.noreply.github.com>
2022-03-24 19:59:28 -0700

    Added Events Page

---
+++ app/src/main/java/com/bcit/project_genuity/EventPageActivity.java
@@ -0,0 +1,16 @@
+package com.bcit.project_genuity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_page);
+
+
+    }
+}
